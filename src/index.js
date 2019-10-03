f8102c0efe9326af15f62f0c682f075fd2fd4c5a
ReginaYashina <reginayashina@inbox.ru>
2019-10-03 23:31:43 +0300

    feat: add function decode

--- src/index.js
+++ src/index.js
@@ -38,8 +38,27 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
-}
+    let string = '';
+    
+    let arr = expr.match(/.{10}/g);
+    
+    let arr_dec = arr.map(function (elem) {
+    return elem.replace(/10/g, '.').replace(/11/g, '-').replace(/\*\*\*\*\*\*\*\*\*\*/g, ' ').replace(/^0{1,9}/g,''); 
+    });
+  
+    for (let i=0; i < arr_dec.length; i++){
+    if(arr_dec[i] == ' '){
+      string = string + ' ';
+    } else {
+      for (key in MORSE_TABLE) {
+        if (key == arr_dec[i]) {
+          string = string + MORSE_TABLE[key];
+         };
+        }
+    }
+    }
+    return string
+  }
 
 module.exports = {
     decode
